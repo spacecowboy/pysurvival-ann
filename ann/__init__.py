bd0e55080483b3d8c15fea84e361de5ca3430842
Jonas Kalderstam <jonask@thep.lu.se>
2012-10-07 02:04:36 +0200

    managed to train, woo
    Signed-off-by: Jonas Kalderstam <jonask@thep.lu.se>

--- ann/__init__.py
+++ ann/__init__.py
@@ -1,2 +1,25 @@
 #Expose functionality in c at package level
 from _ann import *
+from random import uniform
+
+def getSingleLayerRProp(numOfInputs, numOfHidden, numOfOutputs):
+    ''' Constructs and connects a neural network with a single layer
+    of hidden neurons which can be trained with rprop.'''
+    net = rpropnetwork(numOfInputs, numOfHidden, numOfOutputs)
+
+    net.setOutputFunction(net.LOGSIG)
+    net.setHiddenFunction(net.TANH)
+
+    for h in range(numOfHidden):
+        net.connectHToB(h, uniform(-0.5, 0.5))
+        for i in range(numOfInputs):
+            net.connectHToI(h, i, uniform(-0.5, 0.5))
+
+
+    for o in range(numOfOutputs):
+        net.connectOToB(o, uniform(-0.5, 0.5))
+        for h in range(numOfHidden):
+            net.connectOToH(o, h, uniform(-0.5, 0.5))
+
+
+    return net
