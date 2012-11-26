d23f61c9305abe02fa321944fff72f7f7043c7e5
Jonas Kalderstam <jonas@kalderstam.se>
2012-11-26 10:24:39 +0100

    Added riskeval to geneticsurvival networks
    Signed-off-by: Jonas Kalderstam <jonas@kalderstam.se>

--- ann/__init__.py
+++ ann/__init__.py
@@ -5,8 +5,34 @@ A neural network package implemented in c++.
 Should write intended usage here...
 """
 
-from ._ann import *
+from ._ann import (ffnetwork, rpropnetwork,
+                   gensurvnetwork as _gensurvnetwork, get_C_index)
 from random import uniform
+import numpy as np
+
+class gensurvnetwork(_gensurvnetwork):
+    '''Genetic survival network.'''
+
+    def riskeval(self, inputdata):
+        '''Given the inputs for a patient, returns its normalized
+        relative rank, compared to the training data.
+
+        Input: An array of equal length as numOfInputs
+        Returns: A value between 0 and 1
+        '''
+        if not hasattr(self, 'trnoutputs'):
+            raise ValueError('You have to train the network first')
+
+        out = self.output(inputdata)
+        # How many have better expected survival
+        idx = len(self.trnoutputs[self.trnoutputs > out])
+        return idx / len(self.trnoutputs)
+
+    def learn(self, trninputs, trntargets):
+        '''Trains the network using a genetic algorithm'''
+        super(gensurvnetwork, self).learn(trninputs, trntargets)
+        # Evaluate and store outputs for training data
+        self.trnoutputs = np.array([self.output(x) for x in trninputs]).ravel()
 
 def getSingleLayerRProp(numOfInputs, numOfHidden, numOfOutputs):
     ''' Constructs and connects a neural network with a single layer
