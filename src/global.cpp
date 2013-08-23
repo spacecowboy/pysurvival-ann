cd60a7fb702d0b7bca761c2a523417720be69c3b
Jonas Kalderstam <jonas@kalderstam.se>
2013-08-23 13:13:05 +0200

    Fix the mutex locking
    Signed-off-by: Jonas Kalderstam <jonas@kalderstam.se>

--- src/global.cpp
+++ src/global.cpp
@@ -16,7 +16,9 @@ std::mutex JGN_mutexPopulation;
 
 
 void JGN_lockPopulation() {
-  std::lock_guard<std::mutex> lock(JGN_mutexPopulation);
+  //std::lock_guard<std::mutex> lock(JGN_mutexPopulation);
+  // Cant use that, because it is released when lock goes out of scope
+  JGN_mutexPopulation.lock();
 }
 
 void JGN_unlockPopulation() {
