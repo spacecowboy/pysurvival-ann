57efb109bf186bb975c6e2f7fc44e438c1b7873b
Jonas Kalderstam <jonas@kalderstam.se>
2014-01-22 11:22:30 +0100

    Adding commit hook that runs tests
    Signed-off-by: Jonas Kalderstam <jonas@kalderstam.se>

---
+++ git-pre-commit.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Dont test code that is not being committed
+git stash -q --keep-index
+
+# Test the commit
+make test
+RESULT=$?
+
+# Restore working dir
+git stash pop -q
+
+# Return exit status
+[ $RESULT -ne 0 ] && exit 1
+exit 0
