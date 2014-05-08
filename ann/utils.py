afca02714a0c65e6479b363cfa927dd96f87ab3d
Jonas Kalderstam <jonas@kalderstam.se>
2014-05-08 11:05:20 +0200

    Normalize weights in feedforward better
    Each layer should sum to 1
    
    Signed-off-by: Jonas Kalderstam <jonas@kalderstam.se>

--- ann/utils.py
+++ ann/utils.py
@@ -26,6 +26,9 @@ def connect_feedforward(net, layers=None, hidden_act=None, out_act=None):
     Additional layers can be specified as follows:
     >>> connect_feedforward(net, [5, 5])
     >>> connect_feedforward(net, [2,3,3,2])
+
+    The weights are randomized and normalized so that each layer's L1-norm
+    sums to 1.
     '''
     if layers is None:
         layers = [net.hidden_count]
@@ -36,23 +39,21 @@ def connect_feedforward(net, layers=None, hidden_act=None, out_act=None):
     # plus one for bias
     dim = net.input_count + net.hidden_count + net.output_count + 1
     conns = net.connections.reshape((dim, dim))
-    weights = net.weights.reshape((dim, dim))
+
     # reset connections
     conns[:, :] = 0
-    weights[:, :] = 0
+
+    # Get some random weights
+    weights = np.random.normal(size=net.weights.shape).reshape((dim, dim))
 
     # connect all to bias
     conns[:, net.input_count] = 1
-    weights[:, net.input_count] = np.random.normal(size=dim)
 
     hidden_start = net.input_count + 1
 
     # connect first hidden layer to inputs
     conns[hidden_start:hidden_start + layers[0],
           0:net.input_count] = 1
-    weights[hidden_start:hidden_start + layers[0],
-            0:net.input_count] = np.random.normal(size=(layers[0],
-                                                        net.input_count))
 
     prev_start = hidden_start
     prev_end = hidden_start + layers[0]
@@ -60,9 +61,7 @@ def connect_feedforward(net, layers=None, hidden_act=None, out_act=None):
     for layer in layers[1:]:
         conns[prev_end:prev_end + layer,
               prev_start:prev_end] = 1
-        weights[prev_end:prev_end + layer,
-                prev_start:prev_end] = np.random.normal(
-                    size=(layer, prev_end - prev_start))
+
         prev_start = prev_end
         prev_end += layer
 
@@ -70,12 +69,27 @@ def connect_feedforward(net, layers=None, hidden_act=None, out_act=None):
     # connect outputs to last hidden layer
     conns[output_start:,
           output_start - layers[-1]:output_start] = 1
-    weights[output_start:,
-            output_start - layers[-1]:output_start] = np.random.normal(
-                size=(net.output_count, layers[-1]))
 
     net.connections = conns.reshape(dim*dim)
-    net.weights = weights.reshape(dim*dim)
+
+    # normalize each layer to have L1-norm = 1
+    lstart = hidden_start
+    lprev_start = 0
+    for l in layers:
+        for n in range(l):
+            row = lstart + n
+            coll, colr = lprev_start, lstart
+            weights[row, coll:colr] /= np.linalg.norm(weights[row, coll:colr],
+                                                       ord=1)
+        lprev_start = lstart
+        lstart += l
+
+    # output normalize
+    for row in range(output_start, output_start + net.output_count):
+        coll, colr = lprev_start, lstart
+        weights[row, coll:colr] /= np.linalg.norm(weights[row, coll:colr],
+                                                  ord=1)
+    net.weights = weights.ravel()
 
     # set activation functions
     act = net.activation_functions
